f9e823410d1714571887f3c4fc93bc3cca0002bd
Ines Munoz <inesmunozarnau@gmail.com>
2021-09-26 11:52:38 +0200

    added post into elasticsearch

--- scripts/ETL.py
+++ scripts/ETL.py
@@ -8,6 +8,8 @@ import json
 from consumer import consumer
 from model import vec
 
+
+
 import pickle
 pickle_filename = "pickle_svm.pkl"
     
@@ -15,10 +17,26 @@ with open(pickle_filename, "rb") as file:
     pickle_model = pickle.load(file)
 
 
+from elasticsearch import Elasticsearch
+import requests
+
+es = Elasticsearch('http://elasticsearch:9200')
+requests.put('http://elasticsearch:9200/logs-iti')
+
+for i in range(size):
+    requests.post('http://elasticsearch:9200/logs-clean/doc/',json=document[str(i)])
+
 for message in consumer:
     obj = json.loads(message.value.decode('utf-8'))
     payload = [obj['payload']]
     
-    res = vec.transform(payload)
-    predition = pickle_model.predict(res)
-    print(predition)
+    res = vec.transform(payload) #vectorize the payload
+    
+    prediction = int(pickle_model.predict(res)) #call the model
+    print(prediction)
+    
+    obj["prediction"] = prediction #add the prediction to the dictionary
+    
+    json_doc = json.dumps(obj, indent = 4)#Convert dict to json
+    
+    requests.post('http://elasticsearch:9200/logs-iti/doc/',json=json_doc) #post into elasticsearch
